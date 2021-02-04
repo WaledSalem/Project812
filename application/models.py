1032770f9cde9dc516873a41be826f628bb302d5
WaledSalem <eng.walied.salem.capw@gmail.com>
2021-02-04 18:06:59 +0000

    classes defined and created

--- application/models.py
+++ application/models.py
@@ -6,7 +6,9 @@ class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(100), nullable=False)
     answer = db.Column(db.String(30), nullable=False)
+    employees = db.relationship('Employees', backref='question')
 
 class Employees(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
--- create.py
+++ create.py
@@ -2,4 +2,6 @@
 
 from application import db
 
+db.session.remove()
+db.drop_all()
 db.create_all()
