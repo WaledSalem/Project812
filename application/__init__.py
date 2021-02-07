1566bb2deb8688b69f92822e40805df6c81fa788
WaledSalem <75454248+WaledSalem@users.noreply.github.com>
2021-02-07 20:10:06 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SOME_KEY'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://waled:root@localhost/data"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['WTF_CSRF_ENABLED'] = False
+db = SQLAlchemy(app)
+
+from application import routes
+
